2feeae94332ed5be17482fcf5154bf18fec1505c
francisohara24 <franciskohara@gmail.com>
2023-09-03 09:33:53 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from smtplib import SMTP
+from email.message import EmailMessage
+
+
+# function for sending graduate decisions
+def undergrad
+
+# function for sending undergraduate decisions
+
+
+# instantiate SMTP client
+
+
+# send grad school decisions
+
+
+# send undergrad decisions
