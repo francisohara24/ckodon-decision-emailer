76afa5413c3189423d98c438c4acd84e35b9f8a4
francisohara24 <franciskohara@gmail.com>
2023-09-04 22:31:56 -0400

    Feat: complete `grad_emailer`, `undergrad_emailer` functions
    Complete functions for sending grad mentorship decisions and undergrad mentorship decisions

--- main.py
+++ main.py
@@ -8,14 +8,30 @@ from email_validator import validate
 
 # function for sending graduate decisions
 def grad_emailer():
-    smtp = start_smtp()  # instantiate smtp client
-    graduate_students = pd.read_excel("./data/Graduate Pairing.xlsx")  # import graduate student data
-    wait_counter = 0  # count no. of emails till delay.
+    # instantiate smtp client
+    smtp = start_smtp()
+
+    # import graduate student data
+    graduate_students = pd.read_excel("./data/Graduate Pairing.xlsx")
+
+    # validate email addresses
+    graduate_emails = graduate_students["Email"]
+    for email in graduate_emails:
+        status = validate(email.strip())
+        if not status:
+            print("Invalid:", email)
+
+    wait_counter = 0  # count no. of emails till waiting time.
+
+    # obtain whatsapp link from credentials.json
+    credentials = json.loads(open("./data/credentials.json").read())
+    grad_whatsapp_link = credentials["grad_whatsapp_link"]
+
+    # create email message
     for row in graduate_students.index:
         name = graduate_students.loc[row]["Full Name"]
         email_address = graduate_students.loc[row]["Email"]
 
-        # create email message
         msg = EmailMessage()
         msg["From"] = "ckodontech@gmail.com"
         msg["To"] = email_address
@@ -26,7 +42,7 @@ Congratulations!
 You've been selected to join the Ckodon 2023 Graduate Application Mentorship Program.
 Join the following WhatsApp group to receive further instructions about your next steps and to confirm your mentor:
 
-https://chat.whatsapp.com/DAeyCTPMirl65UKc54rhvc
+{grad_whatsapp_link}
 
 DO NOT SHARE this link with anyone.
 
@@ -39,8 +55,10 @@ The Ckodon Foundation Team."""
             smtp.send_message(msg)
             print("SENT")
             wait_counter += 1
-        except:
+        except Exception as exception:
+            print(exception)
             print(row + 2, name, email_address)
+            break
 
         # wait 1 minute every 50 emails
         if wait_counter == 50:
@@ -49,8 +67,62 @@ The Ckodon Foundation Team."""
 
 # function for sending undergraduate decisions
 def undergrad_emailer():
-    pass
+    smtp = start_smtp()  # instantiate smtp client
 
+    # import student data
+    undergraduate_students = pd.read_excel("./data/Undergraduate Pairing.xlsx")
+
+    # validate email addresses
+    undergrad_emails = undergraduate_students["Email"]
+    for email in undergrad_emails:
+        status = validate(email.strip())
+        if not status:
+            print("Invalid:", email)
+
+    # obtain whatsapp link from credentials.json
+    credentials = json.loads(open("./data/credentials.json").read())
+    undergrad_whatsapp_link = credentials["undergrad_whatsapp_link"]
+
+    wait_counter = 0  # count no. of emails sent till waiting time.
+
+    # create email message
+    for row in undergraduate_students.index:
+        name = undergraduate_students.loc[row]["Full Name"]
+        email_address = undergraduate_students.loc[row]["Email"]
+        msg = EmailMessage()
+        msg["From"] = "ckodontech@gmail.com"
+        msg["To"] = email_address
+        msg["Subject"] = "Ckodon Mentorship Program Update"
+        body = f"""Dear {name},
+
+Congratulations!
+You've been selected to join the Ckodon 2023 Graduate Application Mentorship Program.
+Your mentorship group will be focused on building your undergraduate profile in order to prepare you to put forth a competitive application during the next application season.
+Join the following WhatsApp group to learn more about your next steps and connect with your mentor:
+
+{undergrad_whatsapp_link}
+
+DO NOT SHARE this link with anyone.
+
+Best,
+The Ckodon Foundation Team.
+"""
+        msg.set_content(body)
+
+        # send email message
+        try:
+            smtp.send_message(msg)
+            print("SENT")
+            wait_counter += 1
+        except Exception as exception:
+            print(exception)
+            print(row + 2, name, email_address)
+            break
+
+        # wait 1 minute for every 50 emails
+        if wait_counter == 50:
+            wait_counter = 0
+            wait()
 
 # function for instantiating SMTP client
 def start_smtp():
@@ -72,11 +144,5 @@ def wait():
     sleep(duration_s)
 
 
-grad_emailer()
-
-# for email_address in graduate_student_emails:
-#     # validate email addresses
-#     status = validate(email_address.strip())
-#     if not status:
-#         print(status, email_address)  # print all invalid email addresses
-    # grad_emailer(email_address, smtp)
+# grad_emailer()
+# undergrad_emailer()
