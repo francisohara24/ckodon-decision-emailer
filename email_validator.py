b24b90273df39ae111b03b96bdf06c3c558ef959
francisohara24 <franciskohara@gmail.com>
2023-09-04 22:30:37 -0400

    Feat: complete `validate` function for validating email addresses.

--- email_validator.py
+++ email_validator.py
@@ -1,1 +1,6 @@
-import re
+from re import search
+
+
+def validate(email_address):
+    pattern = r"^[A-Za-z0-9_\-.+]+@[a-zA-Z0-9.\-_+]+[A-Za-z]{2,}$"
+    return bool(search(pattern, email_address))
