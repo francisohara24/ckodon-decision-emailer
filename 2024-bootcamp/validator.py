2040b0449b2ef3c1e3d580cf7fa5629dea2fe677
francisohara24 <franciskohara@gmail.com>
2024-05-12 16:13:56 -0400

    Docs: add documentation for `validate()` method.

--- 2024-bootcamp/validator.py
+++ 2024-bootcamp/validator.py
@@ -2,18 +2,31 @@
 import re
 import pandas as pd
 
-students = pd.read_excel("./data/Ckodon Bio Submission Form (Responses).xlsx")
-students = students.tail(len(students) - 1265)
-print(students["Full Name"])
+# import student data
+PG800_UG700A = pd.read_excel("./data/PG800+UG700A.xlsx")
+PG884_UG700B = pd.read_excel("./data/PG884+UG700B.xlsx")
+UG675 = pd.read_excel("./data/UG675.xlsx")
+
+# verify imported data
+print("PG800+UG700A:\n", PG800_UG700A.head())
+print("PG884_UG700B:\n", PG884_UG700B.head())
+print("UG675:\n", UG675.head())
 
 
 def validate(email: str)-> bool:
-    is_valid = False
-    regex = "[a-zA-Z0-9.\-+_]+@[a-zA-Z0-9.\-+_]+[a-zA-Z]{2,}"
+    """Returns a boolean indicating whether the given email is a valid email address.
+
+    Args:
+        email: The email address to be validated.
+
+    Returns:
+        `True` if the given email is a valid email address. Otherwise, returns `False`.
+    """
+    regex = "[a-zA-Z0-9.\-+_]+@[a-zA-Z0-9.\-+_]+[a-zA-Z]{2,}"  # regex pattern for validating address
     email = email.strip()  # remove leading and trailing whitespace
     if re.search(regex, email):
-        is_valid = True
-    return is_valid
+        return True
+    return False
 
 
 for email in students['Email']:
